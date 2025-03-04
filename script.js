0c9ce274aa471deb22783a688c65efb9f8454feb
Владислав Щемелинин <shchemelvladislav@gmail.com>
2025-03-04 19:37:33 +0300

    fix

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-document.getElementById('showTimeButton').addEventListener('click', function() {
-    window.location.href = 'time.html';  // Перенаправляем на страницу с временем
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('showTimeButton').addEventListener('click', function() {
+        window.location.href = 'time.html';  // Перенаправляем на страницу с временем
+    });
 });
+
